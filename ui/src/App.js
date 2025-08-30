83255802e4c36896f626d23780c9efa65523b6cf
bcrabbe <ben.crabbe.dev@gmail.com>
2025-08-30 13:58:35 +0800

    UI: clear card player thinking state when given prompt to play card

--- ui/src/App.js
+++ ui/src/App.js
@@ -118,6 +118,7 @@ const App = () => {
         setSelectedDiscardCards([]);
         // Clear regular prompt
         setPrompt(null);
+        setCurrentCardPlayer(null);
         break;
 
       case 'bid_prompt':
